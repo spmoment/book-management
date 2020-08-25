58540db6c14358f8ff522291f412934f4e6ba68f
Stepan Rodionov <stwork93@mail.ru>
2020-08-25 18:15:06 +0300

    change file scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -2,19 +2,19 @@ CREATE DATABASE book_db;
 
 CREATE TABLE authors
     (id SERIAL PRIMARY KEY,
-    first_name CHARACTER VARYING(30),
-    last_name CHARACTER VARYING(30),
-    year_of_birth SMALLINT);
+    first_name CHARACTER VARYING(50),
+    last_name CHARACTER VARYING(50),
+    year_of_birth DATE);
 
 CREATE TABLE books
     (id SERIAL PRIMARY KEY,
-    title CHARACTER VARYING(30),
-    year_publishing SMALLINT,
-    annotation CHARACTER VARYING(30));
+    title CHARACTER VARYING(50),
+    year_publishing DATE,
+    annotation CHARACTER VARYING(1000));
 
 CREATE TABLE books_authors
-    (books_id SERIAL,
-    authors_id SERIAL,
+    (books_id SMALLINT,
+    authors_id SMALLINT,
     PRIMARY KEY(books_id, authors_id),
     FOREIGN KEY (books_id) REFERENCES books (id),
     FOREIGN KEY (authors_id) REFERENCES authors (id));
