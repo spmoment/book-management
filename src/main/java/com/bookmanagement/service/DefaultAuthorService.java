6d503232f1ab74dcbacf3e662e1da2aaa0bb2d75
Stepan Rodionov <stwork93@mail.ru>
2020-09-01 11:00:37 +0300

    added log.info

--- src/main/java/com/bookmanagement/service/DefaultAuthorService.java
+++ src/main/java/com/bookmanagement/service/DefaultAuthorService.java
@@ -3,9 +3,11 @@ package com.bookmanagement.service;
 import com.bookmanagement.dto.AuthorsDto;
 import com.bookmanagement.entity.Authors;
 import com.bookmanagement.repository.AuthorsRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import javax.xml.bind.ValidationException;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import static java.util.Objects.isNull;
@@ -16,36 +18,43 @@ public class DefaultAuthorService implements AuthorService {
 
     private AuthorsRepository authorsRepository;
     private AuthorsConverter authorsConverter;
-
-    private void validateAuthorsDto(AuthorsDto authorsDto) throws ValidationException {
-        if (isNull(authorsDto)) {
-            throw new ValidationException("Object user is null");
-        }
-        if (isNull(authorsDto.getId()) ) {
-            throw new ValidationException("Id is empty");
-        }
-    }
+    private static final Logger log = (Logger) LoggerFactory.getLogger(DefaultAuthorService.class);
 
     @Override
     public AuthorsDto saveAuthor(AuthorsDto authorsDto) throws ValidationException {
         validateAuthorsDto(authorsDto);
+        log.info("method: saveAuthor");
         Authors savedAuthors = authorsRepository.save(authorsConverter.fromAuthorsDtoToAuthors(authorsDto));
         return authorsConverter.fromAuthorsToAuthorsDto(savedAuthors);
     }
 
+    private void validateAuthorsDto(AuthorsDto authorsDto) throws ValidationException {
+        log.info("method: validateAuthorsDto, before if (isNull(authorsDto))");
+        if (isNull(authorsDto)) {
+            throw new ValidationException("Object user is null");
+        }
+        log.info("method: validateAuthorsDto, before if (isNull(authorsDto.getLastName()))");
+        if (isNull(authorsDto.getLastName())) {
+            throw new ValidationException("Lastname is empty");
+        }
+    }
+
     @Override
     public AuthorsDto updateAuthor(AuthorsDto updateAuthor) {
+        log.info("method: updateAuthor");
         Authors updateAuthors = authorsRepository.save(authorsConverter.fromAuthorsDtoToAuthors(updateAuthor));
         return authorsConverter.fromAuthorsToAuthorsDto(updateAuthors);
     }
 
     @Override
     public void deleteAuthor(Integer id) {
+        log.info("method: deleteAuthor");
         authorsRepository.deleteById(id);
     }
 
     @Override
     public List<AuthorsDto> findAllAuthors() {
+        log.info("method: findAllAuthors");
         return authorsRepository.findAll()
                 .stream()
                 .map(authorsConverter::fromAuthorsToAuthorsDto)
@@ -54,6 +63,7 @@ public class DefaultAuthorService implements AuthorService {
 
     @Override
     public AuthorsDto findAuthorBySurname(String surname) {
+        log.info("method: findAuthorBySurname");
         Authors authors = authorsRepository.findAuthorBySurname(surname);
         if(authors != null)
             return authorsConverter.fromAuthorsToAuthorsDto(authors);
--- src/main/java/com/bookmanagement/service/DefaultBookService.java
+++ src/main/java/com/bookmanagement/service/DefaultBookService.java
@@ -3,10 +3,11 @@ package com.bookmanagement.service;
 import com.bookmanagement.dto.BooksDto;
 import com.bookmanagement.entity.Books;
 import com.bookmanagement.repository.BooksRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
 import javax.xml.bind.ValidationException;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import static java.util.Objects.isNull;
@@ -16,25 +17,27 @@ public class DefaultBookService implements BookService{
 
     private BooksRepository booksRepository;
     private BooksConverter booksConverter;
+    private static final Logger log = (Logger) LoggerFactory.getLogger(DefaultBookService.class);
 
-    private void validateBooksDto(BooksDto booksDto) throws ValidationException {
-        if (isNull(booksDto)) {
-            throw new ValidationException("Object user is null");
-        }
-        if (isNull(booksDto.getId()) ) {
-            throw new ValidationException("Id is empty");
-        }
-    }
 
     @Override
     public BooksDto saveBook(BooksDto booksDto) throws ValidationException {
         validateBooksDto(booksDto);
+        log.info("method: saveBook");
         Books savedBooks = booksRepository.save(booksConverter.fromBooksDtoToBooks(booksDto));
         return booksConverter.fromBooksToBooksDto(savedBooks);
     }
 
+    private void validateBooksDto(BooksDto booksDto) throws ValidationException {
+        log.info("method: validateBooksDto, before if (isNull(booksDto))");
+        if (isNull(booksDto)) {
+            throw new ValidationException("Object user is null");
+        }
+    }
+
     @Override
     public BooksDto updateAuthor(BooksDto updateBook) {
+        log.info("method: updateAuthor");
         Books updateBooks = booksRepository.save(booksConverter.fromBooksDtoToBooks(updateBook));
         return booksConverter.fromBooksToBooksDto(updateBooks);
     }
@@ -46,6 +49,7 @@ public class DefaultBookService implements BookService{
 
     @Override
     public List<BooksDto> findAllBooks() {
+        log.info("method: deleteBook");
         return booksRepository.findAll()
                 .stream()
                 .map(booksConverter::fromBooksToBooksDto)
@@ -54,6 +58,7 @@ public class DefaultBookService implements BookService{
 
     @Override
     public BooksDto findBookById(Integer id) {
+        log.info("method: findBookById");
         Books books = booksRepository.findBookById(id);
         if(books != null)
             return booksConverter.fromBooksToBooksDto(books);
