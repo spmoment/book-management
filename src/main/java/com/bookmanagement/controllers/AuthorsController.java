96f1f90eb34db3f53998a4cf3d8741102975888b
spmoment <63513687+spmoment@users.noreply.github.com>
2020-09-25 20:10:36 +0300

    swagger (#12)

--- src/main/java/com/bookmanagement/controllers/AuthorsController.java
+++ src/main/java/com/bookmanagement/controllers/AuthorsController.java
@@ -4,6 +4,8 @@ import com.bookmanagement.dto.AuthorsDto;
 import com.bookmanagement.exception.NotFoundException;
 import com.bookmanagement.exception.ValidationException;
 import com.bookmanagement.service.AuthorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.logging.Logger;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
+@Api(description = "Operations pertaining to authors in Online Store")
 public class AuthorsController {
 
     private static final Logger log = Logger.getLogger("AuthorsController.class");
@@ -21,18 +24,21 @@ public class AuthorsController {
     AuthorService authorService;
 
     @PostMapping("/saveAuthor")
+    @ApiOperation(value = "Create a author", response = AuthorsDto.class)
     AuthorsDto saveAuthor(@RequestBody AuthorsDto authorsDto) throws ValidationException {
         log.info("handling create author request: " + authorsDto);
         return authorService.saveAuthor(authorsDto);
     }
 
     @PutMapping("/updateAuthor")
+    @ApiOperation(value = "Update a author", response = AuthorsDto.class)
     AuthorsDto updateAuthor(@RequestBody AuthorsDto authorsDto) throws NotFoundException {
         log.info("handling update author request: " + authorsDto);
         return authorService.updateAuthor(authorsDto);
     }
 
     @DeleteMapping("/deleteAuthor/{id}")
+    @ApiOperation(value = "Delete a author", response = Integer.class)
     ResponseEntity<Void> deleteAuthor(@PathVariable Integer id) {
         log.info("handling delete author request: id=" + id);
         authorService.deleteAuthor(id);
@@ -40,12 +46,14 @@ public class AuthorsController {
     }
 
     @GetMapping("/findByLastName")
+    @ApiOperation(value = "Find author by last name", response = AuthorsDto.class)
     AuthorsDto findByLastName(@RequestParam String lastName) {
         log.info("handling findByLastName request: " + lastName);
         return authorService.findAuthorByLastName(lastName);
     }
 
     @GetMapping("/findAllAuthors")
+    @ApiOperation(value = "Find all authors", response = AuthorsDto.class)
     List<AuthorsDto> findAllAuthors() {
         log.info("handling findAllAuthors request");
         return authorService.findAllAuthors();
--- src/main/java/com/bookmanagement/controllers/BooksController.java
+++ src/main/java/com/bookmanagement/controllers/BooksController.java
@@ -4,6 +4,8 @@ import com.bookmanagement.dto.BooksDto;
 import com.bookmanagement.exception.NotFoundException;
 import com.bookmanagement.exception.ValidationException;
 import com.bookmanagement.service.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.logging.Logger;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
+@Api(description = "Operations pertaining to books in Online Store")
 public class BooksController {
 
     private static final Logger log = Logger.getLogger("BooksController.class");
@@ -21,18 +24,21 @@ public class BooksController {
     BookService bookService;
 
     @PostMapping("/saveBook")
+    @ApiOperation(value = "Create a book", response = BooksDto.class)
     BooksDto saveBook(@RequestBody BooksDto booksDto) throws ValidationException {
         log.info("handling create book request: " + booksDto);
         return bookService.saveBook(booksDto);
     }
 
     @PutMapping("/updateBook")
+    @ApiOperation(value = "Update a book", response = BooksDto.class)
     BooksDto updateBook(@RequestBody BooksDto booksDto) throws NotFoundException {
         log.info("handling update book request: " + booksDto);
         return bookService.updateBook(booksDto);
     }
 
     @DeleteMapping("/deleteBook/{id}")
+    @ApiOperation(value = "Delete a book", response = Integer.class)
     ResponseEntity<Void> deleteBook(@PathVariable Integer id) {
         log.info("handling delete book request: id=" + id);
         bookService.deleteBook(id);
@@ -40,12 +46,14 @@ public class BooksController {
     }
 
     @GetMapping("/findBookById")
+    @ApiOperation(value = "Find book by id", response = BooksDto.class)
     BooksDto findBookById(@RequestParam Integer id) {
         log.info("handling findBookById request: id=" + id);
         return bookService.findBookById(id);
     }
 
     @GetMapping("/findAllBooks")
+    @ApiOperation(value = "Find all books", response = List.class)
     List<BooksDto> findAllBooks() {
         log.info("handling findAllBooks request");
         return bookService.findAllBooks();
