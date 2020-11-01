aa6e52b893bd5e791c220a6f7245ff177501ff47
Stepan Rodionov <stwork93@mail.ru>
2020-11-01 15:03:26 +0300

    8

--- src/main/resources/static/constant.js
+++ src/main/resources/static/constant.js
@@ -1,3 +1,3 @@
-var url = "http://spring-book-management.herokuapp.com";
+var url = "http://ec2-46-137-100-204.eu-west-1.compute.amazonaws.com:5432/";
 var USER_TOKEN_KEY = 'token';
 var PHONE_NUMBER = 'phone';
